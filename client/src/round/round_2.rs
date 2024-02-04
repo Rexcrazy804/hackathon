c3b0682794d15c3d181db650f8f5ee08bc25da4a
Rexie Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-02-04 07:00:52 +0530

    tests and resolutions for round 2

--- client/src/round/round_2.rs
+++ client/src/round/round_2.rs
@@ -1,22 +1,36 @@
+// sum of Largest sequence(s) of ascending order numbers
+/*
+* iterate over each line for each line find the sequence that has more than 1 member that is in the
+* ansending order. then find the sum of the digits in the said sequence do this for each line
+* return the grand sum
+*/
+
 use std::cmp::Ordering;
 pub(super) fn compute(input_string: &str) -> u32 {
     let mut sum_of_sequence_digits = 0;
+    // println!("{:?}", input_string.clone().lines().map(|s| s.to_string()).collect::<Vec<String>>());
 
     for line in input_string.lines() {
         let mut current_sequence: Vec<u32> = Vec::new();
         let mut highest_length = 1;
         let mut line_sum = 0;
 
-        for num in line.chars().map(|x| x.to_digit(10).unwrap()) {
-            // num is alwasy greater than zero in the event that the sequence is empty we will
-            // force the if condition to be true by imposing num > 0
-            if &num > current_sequence.last().unwrap_or(&0) {
+        // the concatination ensures that the last iteration will force a sequence check
+        for num in (line.to_owned() + "0").chars().map(|x| x.to_digit(10).unwrap()) {
+            // println!("num: {num}, sum: {line_sum}, sequence: {sum_of_sequence_digits}");
+            if current_sequence.is_empty() {
+                current_sequence.push(num);
+                continue
+            }
+            if &num >= current_sequence.last().unwrap() {
                 current_sequence.push(num);
             } else {
                 match current_sequence.len().cmp(&highest_length) {
                     Ordering::Less => (),
                     Ordering::Equal => {
-                        line_sum += current_sequence.iter().sum::<u32>();
+                        if highest_length != 1 {
+                            line_sum += current_sequence.iter().sum::<u32>();
+                        }
                     },
                     Ordering::Greater => {
                         line_sum = current_sequence.iter().sum::<u32>();
--- client/src/round/round_tests.rs
+++ client/src/round/round_tests.rs
@@ -1,9 +1,22 @@
+use super::round_1;
+use super::round_2;
+
 #[test]
 fn round_1_test() {
-    assert_eq!(super::round_1::compute("1asjkldfalk;jljag;"), 11);
-    assert_eq!(super::round_1::compute("1tgow2kajsd9kjlas"), 19);
-    assert_eq!(super::round_1::compute("001010101010101"), 1);
-    assert_eq!(super::round_1::compute("abab"), 0);
-    assert_eq!(super::round_1::compute("aaaa09"), 9);
-    assert_eq!(super::round_1::compute("9AL:Kjg123"), 93);
+    assert_eq!(round_1::compute("1asjkldfalk;jljag;"), 11);
+    assert_eq!(round_1::compute("1tgow2kajsd9kjlas"), 19);
+    assert_eq!(round_1::compute("001010101010101"), 1);
+    assert_eq!(round_1::compute("abab"), 0);
+    assert_eq!(round_1::compute("aaaa09"), 9);
+    assert_eq!(round_1::compute("9AL:Kjg123"), 93);
+}
+
+#[test]
+fn round_2_test() {
+    assert_eq!(round_2::compute("19182316789"), (1+6+7+8+9));
+    assert_eq!(round_2::compute("1212"), (1+2+1+2)); 
+    // ignore sequences with singular members
+    assert_eq!(round_2::compute("987654321"), 0);
+    // esnure sequneces at the tail are parsed
+    assert_eq!(round_2::compute("99999999999"), (9+9+9+9+9+9+9+9+9+9+9));
 }
