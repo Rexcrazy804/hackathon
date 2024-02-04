5857dc39b166d3efa6f17834235163918be08684
Rexie Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-02-04 07:13:43 +0530

    some changes to creators

--- server/src/creators.rs
+++ server/src/creators.rs
@@ -3,23 +3,26 @@ use rand::{Rng, random};
 use std::fs::File;
 use std::io::Write;
 
+const MIN_CHARS: u32 = 10;
+const MAX_CHARS: u32 = 40;
+const MIN_LINES: u32 = 4200;
+const MAX_LINES: u32 = 6969;
+
 pub fn create_round_1(team_path: &str) {
-    const MIN_CHARS: u32 = 6;
-    const MAX_CHARS: u32 = 30;
-    const MAX_LINES: u32 = 6969;
     const MIN_NUMBER_RATE: f32 = 0.25;
     const MAX_NUMBER_RATE: f32 = 0.75;
 
     let mut rng = rand::thread_rng();
     let mut output = String::new();
 
-    for _ in 0..MAX_LINES {
+    let lines = rng.gen_range(MIN_LINES..=MAX_LINES);
+    for _ in 0..lines {
         let number_rate = rng.gen_range(MIN_NUMBER_RATE..=MAX_NUMBER_RATE);
         let characters = rng.gen_range(MIN_CHARS..=MAX_CHARS);
 
         for _ in 0..characters {
             if random::<f32>() <= number_rate { // Number
-                output += &rng.gen_range(1..10).to_string();
+                output += &rng.gen_range(0..10).to_string();
             } else { // CHaracter
                 match rng.gen_range(1..=2) {
                     1 => output += &rng.gen_range('a'..='z').to_string(),
@@ -45,14 +48,12 @@ fn writer(team_path: &str, output: String, round_num: u8) {
 }
 
 pub fn create_round_2(team_path: &str) {
-    const MAX_LINES: u32 = 6969;
-    const MIN_CHARS: u32 = 6;
-    const MAX_CHARS: u32 = 30;
 
     let mut rng = rand::thread_rng();
     let mut output = String::new();
 
-    for _ in 0..MAX_LINES {
+    let lines = rng.gen_range(MIN_LINES..=MAX_LINES);
+    for _ in 0..lines {
         let characters = rng.gen_range(MIN_CHARS..MAX_CHARS);
         for _ in 0..characters {
             output += &rng.gen_range(1..10).to_string();
