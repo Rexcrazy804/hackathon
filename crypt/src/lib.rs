c01432412b26d482705670a1d33b43bd92e57682
Rexie Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-02-10 10:28:53 +0530

    made a crypt a library

--- crypt/src/lib.rs
+++ crypt/src/lib.rs
@@ -7,10 +7,10 @@ mod crypt_test;
 use encode::*;
 use decode::*;
 
-fn encoder(input: &str) -> String {
+pub fn encoder(input: &str) -> String {
     encode_2(&encode_1(input))
 }
 
-fn decoder(input: &str) -> String {
+pub fn decoder(input: &str) -> String {
     decode_1(&decode_2(input))
 }
